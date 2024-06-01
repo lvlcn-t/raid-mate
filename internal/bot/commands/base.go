6f4e421c749d70343486f3b47b55882f6aeb162d
lvlcn-t <75443136+lvlcn-t@users.noreply.github.com>
2024-06-01 22:29:30 +0200

    docs: add godocs to command base

--- internal/bot/commands/base.go
+++ internal/bot/commands/base.go
@@ -2,6 +2,7 @@ package commands
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/disgoorg/disgo/events"
 )
@@ -23,6 +24,20 @@ type Command[T Event] interface {
 // Base is a common base for all commands.
 // It should not be executed but rather embedded in other commands.
 // The embedder may only implement command specific methods.
+//
+// Example:
+//
+//	type MyCommand struct {
+//	    *Base[*events.ApplicationCommandInteractionCreate]
+//	}
+//
+//	func NewMyCommand() *MyCommand {
+//	    return &MyCommand{Base: NewBase("my-command")}
+//	}
+//
+//	func (c *MyCommand) Handle(ctx context.Context, event *events.ApplicationCommandInteractionCreate) {
+//	    // handle the command
+//	}
 type Base[T Event] struct {
 	// name is the name of the command.
 	name string
@@ -31,6 +46,12 @@ type Base[T Event] struct {
 // Name returns the name of the command.
 func (c *Base[T]) Name() string {
 	return c.name
+}
+
+// Handle is the handler for the command that is called when the event is triggered.
+// This is a default implementation that panics if not overridden.
+func (c *Base[T]) Handle(_ context.Context, _ T) {
+	panic(fmt.Sprintf("command %q does not have a handler", c.Name()))
 }
 
 // NewBase creates the common base for all commands.
@@ -44,6 +65,7 @@ func NewBase[T Event](name string) *Base[T] {
 	return &Base[T]{name: name}
 }
 
+// toPtr returns a pointer to the given value.
 func toPtr[T any](v T) *T {
 	return &v
 }
